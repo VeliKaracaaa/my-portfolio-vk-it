d5026d9dcbbd56d50cb032621a0729c72bc0354c
veli karaca <veli.karaca@epitech.eu>
2025-07-10 14:29:49 +0200

    install next js and create grid for the first user page

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="grid grid-cols-2 grid-rows-2 gap-6">
+        <h1 className="col-span-2">Développeur Full stack vk-IT</h1>
+        <div className="row-span-2 text-amber-900 bg-red-50">
+          CV & COMPÉTENCES
+        </div>
+        <div className="text-amber-900 bg-red-50">PROJETS</div>
+        <div className="text-amber-900 bg-red-50">EN SAVOIR PLUS</div>
+      </div>
+    </main>
+  );
+}
