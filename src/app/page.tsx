edfe744df4a5b2c3940b365a54a9ffa4b70bfec5
veli karaca <veli.karaca@epitech.eu>
2025-07-23 14:38:48 +0200

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import Image from "next/image";
+import CatImage from "../../public/cat.png";
+import DesktopImage from "../../public/desktop.jpg";
+import MegaphoneImage from "../../public/megaphone.png";
+
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-2 grid-rows-2 gap-6">
-        <h1 className="col-span-2">Développeur Full stack vk-IT</h1>
-        <div className="row-span-2 text-amber-900 bg-red-50">
-          CV & COMPÉTENCES
+      <div className="sm:grid grid-cols-2 grid-rows-2">
+        <div className="row-span-2">
+          <Image src={CatImage} alt="Chat avec des écouteurs et des lunettes" />
         </div>
-        <div className="text-amber-900 bg-red-50">PROJETS</div>
-        <div className="text-amber-900 bg-red-50">EN SAVOIR PLUS</div>
+        <div>
+          <Image src={DesktopImage} alt="desktop image" />
+        </div>
+        <div>
+          <Image src={MegaphoneImage} alt="megaphone image" />
+        </div>
       </div>
     </main>
   );
