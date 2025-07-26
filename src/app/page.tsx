4c226d058cfaaa44996a7d41d2178db67096348f
veli karaca <veli.karaca@epitech.eu>
2025-07-26 18:31:08 +0200

    Hero section modify component ImageCard for sm and md screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,39 +7,48 @@ export default function Home() {
   return (
     <main>
       <section>
-        <div className="grid grid-cols-2 gap-4 mx-auto p-6">
+        <div className="sm:grid sm:grid-cols-2 sm:gap-4 mx-auto p-6">
           <div className="bg-white col-span-2">
-            <h1 className="text-3xl px-6 py-4 text-center font-black text-black">
-              Développeur Full satck <span className="text-black">vk-IT</span>
+            <h1 className="text-4xl px-6 py-4 text-center font-black text-black">
+              Développeur Full Satck <span className="text-black">vk-IT</span>
             </h1>
           </div>
 
           <ImageCard
             href="/cv"
-            linkText="CV & COMPÉTENCES"
+            Text="CV & COMPÉTENCES"
             image={CatImage}
             imageAlt="Chat avec des écouteurs et des lunettes"
-            topPositionSm="top-10"
-            leftPositionSm="left-3"
+            topPosition="sm:top-10 md:top-15"
+            leftPosition="sm:left-3"
+            paddingX="sm:px-2 md:px-2"
+            paddingY="sm:py-2 md:px-2"
+            sizeText="sm:text-2xl md:text-3xl "
           />
 
           <div className="grid grid-rows-2 gap-4">
             <ImageCard
               href="/cv"
-              linkText="Projets"
+              Text="Projets"
               image={DesktopImage}
               imageAlt="Chat avec des écouteurs et des lunettes"
-              topPositionSm="top-60"
-              leftPositionSm="left-2"
+              topPosition="sm:top-60 md:top-75"
+              leftPosition="sm:left-2 md:left-3"
+              paddingX="sm:px-2 md:px-4"
+              paddingY="sm:py-2 md:px-4"
+              sizeText="sm:text-2xl md:text-3xl "
             />
 
             <ImageCard
               href="/cv"
-              linkText="En savoir plus"
+              Text="En savoir plus"
               image={MegaphoneImage}
               imageAlt="Chat avec des écouteurs et des lunettes"
-              topPositionSm="top-60"
-              leftPositionSm="left-2"
+              topPosition="sm:top-60 md:top-75"
+              leftPosition="sm:left-2 md:left-3"
+              paddingX="sm:px-2 md:px-4"
+              paddingY="sm:py-2 md:px-4"
+              sizeText="sm:text-2xl md:text-3xl "
             />
           </div>
         </div>
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,28 +3,34 @@ import Link from "next/link";
 
 interface ImageCardProps {
   href: string;
-  linkText: string;
+  Text: string;
   image: StaticImageData;
   imageAlt: string;
-  topPositionSm?: string; // Optionnel avec '?'
-  leftPositionSm?: string; // Optionnel avec '?'
+  topPosition?: string; // Optionnel avec '?'
+  leftPosition?: string; // Optionnel avec '?'
+  paddingX?: string; // Optionnel avec '?'
+  paddingY?: string; // Optionnel avec '?'
+  sizeText?: string; // Optionnel avec '?'
 }
 
 export default function ImageCard({
   href,
-  linkText,
+  Text,
   image,
   imageAlt,
-  topPositionSm = "top-14", // Valeur par défaut
-  leftPositionSm = "left-3", // Valeur par défaut
+  topPosition = "top-14", // Valeur par défaut
+  leftPosition = "left-3", // Valeur par défaut
+  paddingX = "px-4", // Valeur par défaut
+  paddingY = "py-2", // Valeur par défaut
+  sizeText = "text-2xl", // Valeur par défaut
 }: ImageCardProps) {
   return (
     <div className="relative flex flex-col space-y-4">
       <Link href={href}>
         <div
-          className={`absolute ${topPositionSm} ${leftPositionSm} cursor-pointer px-2 py-2 text-2xl font-extrabold text-purple-800 bg-gradient-to-r from-[#FFB6C1] to-[#BAE6FD] text-center  border border-black shadow-md transition hover:scale-105`}
+          className={`absolute ${topPosition} ${leftPosition} cursor-pointer ${paddingX} ${paddingY} ${sizeText} font-extrabold text-purple-800 bg-gradient-to-r from-[#FFB6C1] to-[#BAE6FD] text-center  border border-black shadow-md transition hover:scale-105`}
         >
-          {linkText}
+          {Text}
         </div>
       </Link>
       <div className="flex-1">
