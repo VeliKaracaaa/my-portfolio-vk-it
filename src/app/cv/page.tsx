32bbfc98e0695a0565c890eb9cf7f5836df195b4
veli karaca <veli.karaca@epitech.eu>
2025-07-30 23:21:32 +0200

    Page Cv ajusted grid with flex and component ImageCv and add Styling Conditionnel class Tailwind in ImageCv component

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -5,24 +5,38 @@ import ImageCv from "@/components/ImageCv";
 
 export default function Cv() {
   return (
-    <main className="container mx-auto grid sm:grid-cols-2">
-      <section className="">
-        <header>
-          <ImageCv image={MeImage} imageAlt="image de moi" />
-          <h1 className="text-7xl font-bold">Veli KARACA</h1>
-          <h1>Dévéloppeur Full Stack</h1>
-          <ImageCv
-            image={linkedinImage}
-            href="https://www.linkedin.com/in/veli-karaca/"
-            Text="Linkedin"
-            imageAlt="image de linkedin"
-          />
-          <ImageCv
-            image={gtihubImage}
-            href="https://github.com/VeliKaracaaa"
-            Text="Github"
-            imageAlt="image de gtihub"
-          />
+    <main className="container mx-auto grid grid-cols-1 sm:grid-cols-[3fr_1fr] gap-5 p-4">
+      <section className="flex flex-col gap-2">
+        <header className="flex gap-2 ">
+          <div className="flex-1">
+            <ImageCv image={MeImage} imageAlt="image de moi" fullWidth={true} />
+          </div>
+          <div className="flex-1 flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Veli KARACA</h1>
+            <h1>Dévéloppeur Full Stack</h1>
+          </div>
+          <div className="flex-1 flex flex-col gap-5 items-center justify-center">
+            <ImageCv
+              image={linkedinImage}
+              href="https://www.linkedin.com/in/veli-karaca/"
+              Text="Linkedin"
+              imageAlt="image de linkedin"
+              width="32"
+              height="32"
+              itemsCenter={true}
+              justifyCenter={true}
+            />
+            <ImageCv
+              image={gtihubImage}
+              href="https://github.com/VeliKaracaaa"
+              Text="Github"
+              imageAlt="image de gtihub"
+              width="32"
+              height="32"
+              itemsCenter={true}
+              justifyCenter={true}
+            />
+          </div>
         </header>
 
         <section>Présentation</section>
--- src/components/ImageCv.tsx
+++ src/components/ImageCv.tsx
@@ -5,19 +5,46 @@ interface ImageCvProps {
   Text?: string;
   href?: string;
   imageAlt: string;
+  width?: number | `${number}`;
+  height?: number | `${number}`;
+  itemsCenter?: boolean;
+  justifyCenter?: boolean;
+  fullWidth?: boolean;
 }
 
-export default function ImageCv({ image, href, Text, imageAlt }: ImageCvProps) {
+export default function ImageCv({
+  image,
+  href,
+  Text,
+  imageAlt,
+  width,
+  height,
+  itemsCenter,
+  justifyCenter,
+  fullWidth,
+}: ImageCvProps) {
   return (
-    <div>
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        {Text}
-      </a>
-      <Image
-        src={image}
-        alt={imageAlt}
-        className="w-full h-full object-cover rounded-lg"
-      />
+    <div
+      className={`flex flex-col ${itemsCenter ? "items-center" : ""} ${
+        justifyCenter ? "justify-center" : ""
+      } gap-1 min-h-0 `}
+    >
+      <div>
+        <a href={href} target="_blank" rel="noopener noreferrer" className="">
+          {Text}
+        </a>
+      </div>
+      <div>
+        <Image
+          src={image}
+          alt={imageAlt}
+          className={`object-cover rounded-lg min-h-0 ${
+            fullWidth ? "w-full h-full" : ""
+          }`}
+          width={width}
+          height={height}
+        />
+      </div>
     </div>
   );
 }
