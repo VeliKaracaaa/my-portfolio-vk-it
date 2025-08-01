32d1a1564527a5a95ed021adfc0c18bf4c020b8b
veli karaca <veli.karaca@epitech.eu>
2025-08-01 13:02:50 +0200

    feature: creation of the presentation section responsive

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -41,9 +41,9 @@ export default function Cv() {
           </div>
         </header>
 
-        <section>
-          <h1>Présentation</h1>
-          <p>
+        <section className="flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md">
+          <h1 className="text-2xl font-bold text-gray-800">Présentation</h1>
+          <p className="text-gray-600 leading-relaxed text-base md:text-lg">
             Diplômé d&apos;une licence en tant que chef de projet web. Je
             souhaite me spécialiser dans le développement web full stack. Je
             recherche donc une alternance pour intégrer un Bachelor avec un
